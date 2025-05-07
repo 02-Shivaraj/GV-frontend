36ea9cc3066905112ab403628237ffdc87cf79b2
Shivaraj <1MS22IS127@msrit.edu>
2025-05-08 00:05:44 +0530

    Update script.js

--- frontend/res/signup/script.js
+++ frontend/res/signup/script.js
@@ -29,7 +29,7 @@ async function signup(event) {
     const password = document.getElementById('password').value;
     const name = document.getElementById('name').value;
 
-    const response = await fetch('/api/signup', {
+    const response = await fetch('/signup', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
