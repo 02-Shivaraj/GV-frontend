be107cead063996ffc510c2fdece0c5156ad6d41
Shivaraj <1MS22IS127@msrit.edu>
2025-05-07 23:55:40 +0530

    routes changes

--- frontend/res/script.js
+++ frontend/res/script.js
@@ -33,7 +33,7 @@ async function login(event) {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('http://localhost:3000/login', {
+    const response = await fetch('/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
